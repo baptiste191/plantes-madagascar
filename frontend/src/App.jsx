fda9b1b21a50f97c4b5e6269bff762a7c49529e2
Baptiste Prevost <baptisteprevost@MacBook-Pro-de-Baptiste.local>
2025-04-26 14:43:23 +0300

    correction boutton déconnexion interface plante

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-// frontend/src/App.jsx
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login       from './pages/Login.jsx'
-import UserHome    from './pages/UserHome.jsx'
-import PlantDetail from './pages/PlantDetail.jsx'
-import AdminHome   from './pages/AdminHome.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
-
-console.log('App chargé, route :', window.location.pathname)
+import Login           from './pages/Login.jsx'
+import UserHome        from './pages/UserHome.jsx'
+import PlantDetail     from './pages/PlantDetail.jsx'
+import AdminHome       from './pages/AdminHome.jsx'
+import ProtectedRoute  from './components/ProtectedRoute.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/plant/:id" element={<PlantDetail />} />
-
-      <Route
-        path="/user"
-        element={
-          <ProtectedRoute roles={['user','admin']}>
-            <UserHome />
-          </ProtectedRoute>
-        }
-      />
 
       <Route
         path="/plant/:id"
         element={
           <ProtectedRoute roles={['user','admin']}>
             <PlantDetail />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/user"
+        element={
+          <ProtectedRoute roles={['user','admin']}>
+            <UserHome />
           </ProtectedRoute>
         }
       />
